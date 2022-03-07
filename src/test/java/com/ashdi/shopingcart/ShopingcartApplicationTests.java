981cc7eb47be2ba6d4c31a0c25e5fb0079dd3aa6
pro100 <frostymag177@gmail.com>
2022-03-07 18:59:41 +0300

    Initial commit

---
+++ src/test/java/com/ashdi/shopingcart/ShopingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashdi.shopingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
